c49d77b21a5392e1d5cd3969231f68af74a3c92d
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-20 19:41:54 +0530

    style: bottom component

--- born-with-luck-app/src/App.js
+++ born-with-luck-app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
+import logo from "./images/logo.svg"
 
 function App() {
+
+  const [close, setClose] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundImage: `url(${logo})`, backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundAttachment: "fixed" }} className="app">
+      <div id="app__containerOne">
+        <div className="app__topContainer app__container">
+          <h1>Is Your Birthday Lucky?</h1>
+          <a href="#app__bottomContainer">Scroll down to check 👇</a>
+        </div>
+      </div>
+
+      <div id="app__containerTwo">
+        <div id="app__bottomContainer" className="app__container">
+          {!close && <div className="app__privacyNotice">
+            <span style={{ fontWeight: "bold" }}>Privacy Notice!</span> We are not storing your data.     <span style={{ cursor: "pointer" }} onClick={() => setClose(true)}>   ❌</span>
+          </div>}
+          <h2>Enter Your Birthdate and lucky number to continue...</h2>
+          <form>
+            <p>Select your Birth date:</p>
+            <input className="app__input" type="date" required />
+            <p>Enter your Lucky Number:</p>
+            <input min="1" className="app__input" type="number" required />
+            <br />
+            <button className="app__button" type="submit">CHECK</button>
+          </form>
+          <footer>
+            <ul>
+              <li><a href="/">Github</a></li>
+              <li><a href="/">LinkedIn</a></li>
+              <li><a href="/">Portfolio</a></li>
+            </ul>
+          </footer>
+        </div>
+      </div>
+
     </div>
   );
 }
