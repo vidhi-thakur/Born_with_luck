972e68f7a09a4dbd91bef4814e6d1dad01c9d4c7
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-21 21:22:35 +0530

    done

--- born-with-luck-app/src/App.js
+++ born-with-luck-app/src/App.js
@@ -1,10 +1,33 @@
 import { useState } from "react"
 import './App.css';
 import logo from "./images/logo.svg"
+import sad from "./images/sad.svg"
+import happy from "./images/happy.svg"
 
 function App() {
 
   const [close, setClose] = useState(false)
+  const [display, setDisplay] = useState(false)
+  const [lucky, setLucky] = useState(false)
+  const [date, setDate] = useState(0)
+  const [number, setNumber] = useState(0)
+
+  const onClickResultHandler = () => {
+    const array = date.split("-")
+    let sum = 0
+    array.map(item => {
+      for (var i = 0; i < item.length; i++) {
+        sum = sum + Number(item[i])
+      }
+    })
+    let result = sum % Number(number)
+    if (result === 0) {
+      setDisplay(true)
+      setLucky(true)
+    } else {
+      setDisplay(true)
+    }
+  }
 
   return (
     <div style={{ backgroundImage: `url(${logo})`, backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundAttachment: "fixed" }} className="app">
@@ -17,23 +40,28 @@ function App() {
 
       <div id="app__containerTwo">
         <div id="app__bottomContainer" className="app__container">
-          {!close && <div className="app__privacyNotice">
+          {!close && <div className="app__privacyNotice app__innerContainer">
             <span style={{ fontWeight: "bold" }}>Privacy Notice!</span> We are not storing your data.     <span style={{ cursor: "pointer" }} onClick={() => setClose(true)}>   ❌</span>
           </div>}
           <h2>Enter Your Birthdate and lucky number to continue...</h2>
           <form>
             <p>Select your Birth date:</p>
-            <input className="app__input" type="date" required />
+            <input value={date} onChange={(e) => setDate(e.target.value)} className="app__input" type="date" required />
             <p>Enter your Lucky Number:</p>
-            <input min="1" className="app__input" type="number" required />
+            <input value={number} onChange={(e) => setNumber(e.target.value)} min="1" className="app__input" type="number" required />
             <br />
-            <button className="app__button" type="submit">CHECK</button>
+            <button className="app__button" type="submit" onClick={onClickResultHandler}>CHECK</button>
           </form>
-          <footer>
-            <ul>
-              <li><a href="/">Github</a></li>
-              <li><a href="/">LinkedIn</a></li>
-              <li><a href="/">Portfolio</a></li>
+          {display && <div>
+            <p style={{ padding: "1.5rem 0px", fontWeight: "bold" }}>{lucky ? `YAY!! You are a lucky person!` : `Oops!! Your birthday is not a lucky number!`}</p>
+            <img style={{ height: "250px" }} src={lucky ? `${happy}` : `${sad}`} alt="result Logo" />
+          </div>}
+          <footer className="app__innerContainer app__footer">
+            <h2 style={{ color: "var(--pink01)", padding: "1rem 0px 0.5rem 0px" }}>CONNECT WITH ME HERE</h2>
+            <ul className="app__footerTable">
+              <li className="app__footerLinks"><a href="https://github.com/vidhi-thakur">Github</a></li>
+              <li className="app__footerLinks"><a href="https://www.linkedin.com/in/vidhi-thakur-9353271a8/">LinkedIn</a></li>
+              <li className="app__footerLinks"><a href="https://portfolio-vidhithakur.netlify.app/">Portfolio</a></li>
             </ul>
           </footer>
         </div>
